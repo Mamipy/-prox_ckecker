4e713a199ac168ff06bd643b9f3d47b5f8c1d720
mamipy <109237839+Mamipy@users.noreply.github.com>
2023-03-23 22:12:24 +0300

    Create proxy_ckecker.py

---
+++ proxy_ckecker.py
@@ -0,0 +1,19 @@
+import requests
+
+with open('proxy.txt', 'r') as f:
+    proxy_list = [line.strip() for line in f]
+
+working_proxies = []
+
+for proxy in proxy_list:
+    try:
+        response = requests.get('https://www.google.com', proxies={'https': proxy}, timeout=1)
+        if response.status_code == 200:
+            working_proxies.append(proxy)
+            with open('çalışan_proxyler.txt', 'a') as f:
+                f.write(proxy + '\n')
+
+    except Exception:
+        pass
+      
+print('Çalışan proxyler:', working_proxies)
